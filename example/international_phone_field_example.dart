5893228a5f49dc94a1a1067b3c47a0649a36705a
Abuvakhobov Isroiljon <65494864+Isroiljoncyber@users.noreply.github.com>
2024-06-22 14:54:11 +0500

    last changes

--- example/international_phone_field_example.dart
+++ example/international_phone_field_example.dart
@@ -24,6 +24,7 @@ class MyApp extends StatelessWidget {
           children: [
             InternationalPhoneTextField(
               onChanged: (number) {},
+              inOneLine: true,
             ),
           ],
         ),
